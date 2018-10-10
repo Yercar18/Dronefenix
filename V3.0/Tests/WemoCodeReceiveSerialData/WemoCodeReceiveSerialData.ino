cb669048b031e70f7c4f61de0fae6c25dec72537
miguel5612 <miguelangelcu@ufps.edu.co>
2018-10-10 18:21:29 -0500

    changed code to mqtt

--- V3.0/Tests/WemoCodeReceiveSerialData/WemoCodeReceiveSerialData.ino
+++ V3.0/Tests/WemoCodeReceiveSerialData/WemoCodeReceiveSerialData.ino
@@ -7,17 +7,28 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <SoftwareSerial.h>
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
 
 #define timeDelay 1400
 #define sep ','
 #define ledWarning 13
 #define BAUD_RATE 115200
 #define chipSelect D4
-#define defaultFileName 'datos'
-#define defaultFileExtension '.csv'
-#define ssid "DroneFenix_V3.00"
-#define password "abcd1234"
+
+const String defaultFileName = "datos";
+const String defaultFileExtension = ".csv";
+const String defaultWebName = "web";
+const String defaultWebExtension = ".html";
+const char* ssid = "DroneFenix_V3.00";
+const char* password = "abcd1234";
+
+//WebPage Settings
+
+const String minWebPageStart = "<!DOCTYPE html><html><head><title>Drone Fenix V3.00</title><style type='text/css'>body{background-color: white;}.pure-table{border-collapse:collapse;border-spacing:0;empty-cells:show;border:1px solid #cbcbcb}.pure-table caption{color:#000;font:italic 85%/1 arial,sans-serif;padding:1em 0;text-align:center}.pure-table td,.pure-table th{border-left:1px solid #cbcbcb;border-width:0 0 0 1px;font-size:inherit;margin:0;overflow:visible;padding:.5em 1em}.pure-table td:first-child,.pure-table th:first-child{border-left-width:0}.pure-table thead{background-color:#e0e0e0;color:#000;text-align:left;vertical-align:bottom}.pure-table td{background-color:transparent}.pure-table-odd td,.pure-table-striped tr:nth-child(2n-1) td{background-color:#f2f2f2}.pure-table-bordered td{border-bottom:1px solid #cbcbcb}.pure-table-bordered tbody>tr:last-child>td{border-bottom-width:0}.pure-table-horizontal td,.pure-table-horizontal th{border-width:0 0 1px;border-bottom:1px solid #cbcbcb}.pure-table-horizontal tbody>tr:last-child>td{border-bottom-width:0}</style></head><body><div style='padding:30px;background-color: blue'><h1 style='color:white;text-align: center;'>ENSAMBLE DE UN DRONE BASADO EN UN SISTEMA SENSORIAL Y MULTIESPECTRAL PARA MEDIR VARIABLES METEOROLÓGICOS</h1></div><h2 style='#'>Desarrolladores:&nbsp&nbsp</h2><ul><li><p style='#'><a href='www.github.com/miguel56121'> Miguel Angel Califa</a> </p></li><li><p><a href='#'>Yersson Carrillo</a></p></li></ul><div><h2 style='clear:both'>Mediciones tomadas por la estacion:</h2><div align='center'><table class='pure-table' style='text-align: center'> <thead> <tr> <th># de muestra tomada</th> <th>Latitud</th><th>Longitud</th><th>Altitud</th><th>Temperatura</th><th>Temperatura CCS</th><th>Temperatura BMP</th><th>Humedad</th><th>Indice de calor</th><th>Dioxido de carbono</th><th>Polvo particulado</th><th>Presion</th> </tr></thead> <tbody>";
+const String minWebPageEnd = "</tbody></table></div><h3 style='background-color:blue;margin:0;margin-top:3%'><div style='padding:1%'></div><a style='color:white' href='/downloadData'>Descargar informacion de la estacion metereologica</a></h3></div><footer style='margin=0;padding:500px;background-color: blue; width: 100%;height: 100%'></footer></body></html>";
+const String startData = "<td>";
+const String endData = "</td>";
+
 
 String Data = "";
 int defaultFileCounter = 0,contadorDatos = 0;
@@ -26,7 +37,9 @@ File Archivo;
 
 
 SoftwareSerial arduinoSerial(D3, D2, false, 256);// SoftwareSerial arduinoSerial(14, 12, false, 256);
-ESP8266WebServer server(80);
+//ESP8266WebServer server(80);
+// Web Server on port 80
+WiFiServer server(80);
 
 void setup() {
   Serial.begin(BAUD_RATE);
@@ -49,7 +62,7 @@ void setup() {
   Serial.print("HotSpt IP:");
   Serial.println(myIP);
   // Starting the web server
-  server.on("/", webPage);  
+  //server.on("/", webPage);  
   server.begin();
   while(SD.exists(String(defaultFileName+defaultFileCounter+defaultFileExtension))){
     Serial.println("EXISTE: "+defaultFileName+defaultFileCounter+defaultFileExtension);
@@ -58,6 +71,12 @@ void setup() {
     delay(defaultFileCounter);
   }
   inicioDeAlmacenamiento(String(defaultFileName+defaultFileCounter+defaultFileExtension));// Esta funcion verifica la integridad del archivo y realiza la insecion de las caveceras
+  //inicio de pagina web
+  Archivo = SD.open(String(defaultWebName+defaultFileCounter+defaultWebExtension), FILE_WRITE);
+  delay(1);
+  Archivo.println(minWebPageStart);
+  delay(1);
+  Archivo.close();
 }
 
 void loop() {
@@ -91,15 +110,25 @@ void saveDataSD(String Data){
     Archivo.println(Data);
     delay(1);
     Archivo.close();
-    Serial.println("Data_almacenada ");
     contadorDatos++;
     Data = "";newData = false;validData = false;
+    //WebUpdate
+    Archivo = SD.open(String(defaultWebName+defaultFileCounter+defaultWebExtension), FILE_WRITE);
+    delay(1);
+    for(int i=0;i<=10;i++){
+      Archivo.println(startData+getValueDouble(Data,sep,i)+endData);
+      delay(1);  
+    }
+    Serial.println("Data_almacenada ");
+    Archivo.close();
   }
+  
   }
 void smartDelay(int timeWait){
   unsigned long oldTime = millis();
   while(millis()-oldTime<=timeWait){
-    server.handleClient();
+    //server.handleClient();
+    WiFiClient client = server.available();
     getDataFromArduino();
     delay(timeWait/10);
   }
@@ -113,7 +142,11 @@ void ValidateAndShowData(){
   if(!validData){Serial.println("Validacion: error");}
 }
 void webPage(){
-            server.send(200, "text/plain",Data);
+            if(!loadFromSdCard("/" + String(defaultWebName+defaultFileCounter+defaultWebExtension))){
+              server.send(200, "text/html","Error loading page");
+            }
+            
+            //server.send(200, "text/plain",Data);
             /*
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
@@ -136,8 +169,8 @@ void inicioDeAlmacenamiento(String fileNameAndExtension){
     // latitud,longitud,altitud,temperatura (DHT), temperatura(CCS), temperatura (BMP), humedad (DHT11), hic (DHT11), co2 (CCS811), tvoc(CCS811), presionAtmosferica (BMP180)
 
     Archivo.println("SEP="+sep);
-    Archivo.print("------------------------------------------------ INICIO DE LA TOMA DE DATOS -------------------------------------------------");
-    Archivo.println();
+    //Archivo.print("------------------------------------------------ INICIO DE LA TOMA DE DATOS -------------------------------------------------");
+    //Archivo.println();
     
     Archivo.print("Latitud");
     Archivo.print(sep);
@@ -192,3 +225,74 @@ bool getValue(String data, char separator, int index)
     }
     return found > index ? data.substring(strIndex[0], strIndex[1]).toInt()>=0 : false;
 }  // END
+
+
+double getValueDouble(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length();
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? atof(data.substring(strIndex[0], strIndex[1]).c_str()) : 0;
+}  // END
+
+bool loadFromSdCard(String path) {
+  String dataType = "text/plain";
+  if (path.endsWith("/")) {
+    path += "index.htm";
+  }
+
+  if (path.endsWith(".src")) {
+    path = path.substring(0, path.lastIndexOf("."));
+  } else if (path.endsWith(".htm")) {
+    dataType = "text/html";
+  } else if (path.endsWith(".css")) {
+    dataType = "text/css";
+  } else if (path.endsWith(".js")) {
+    dataType = "application/javascript";
+  } else if (path.endsWith(".png")) {
+    dataType = "image/png";
+  } else if (path.endsWith(".gif")) {
+    dataType = "image/gif";
+  } else if (path.endsWith(".jpg")) {
+    dataType = "image/jpeg";
+  } else if (path.endsWith(".ico")) {
+    dataType = "image/x-icon";
+  } else if (path.endsWith(".xml")) {
+    dataType = "text/xml";
+  } else if (path.endsWith(".pdf")) {
+    dataType = "application/pdf";
+  } else if (path.endsWith(".zip")) {
+    dataType = "application/zip";
+  }
+
+  File dataFile = SD.open(path.c_str());
+  if (dataFile.isDirectory()) {
+    path += "/index.htm";
+    dataType = "text/html";
+    dataFile = SD.open(path.c_str());
+  }
+
+  if (!dataFile) {
+    return false;
+  }
+
+  if (server.hasArg("download")) {
+    dataType = "application/octet-stream";
+  }
+
+  if (server.streamFile(dataFile, dataType) != dataFile.size()) {
+    Serial.println("Sent less data than expected!");
+  }
+
+  dataFile.close();
+  return true;
+}
+
--- V3.0/WemosCode/WemosCode.ino
+++ V3.0/WemosCode/WemosCode.ino
@@ -1,133 +1,122 @@
 #include <SPI.h>
 #include <SD.h>
-// Including the ESP8266 WiFi library
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <SoftwareSerial.h>
-#include <ESP8266WebServer.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-#define master 1
-#define slave 8
-// Replace with your network details
-const char* ssid = "DroneFenix_V3.00";
-const char* password = "abcd1234";
-
-//Variables que van a ser leidas del arduino
-short p,P,tvoc,co2,alt;
-byte tempCCS,t,T,h,tBMP,hic;
-float lat=0,lon=0;
-
-
-//Definitions
-#define SLAVE_ADDRESS 8 //Slave arduino ID
-#define chipSelect D4 //D4
-#define bytes 100
+#define timeDelay 2000
 #define sep ','
-#define timeDelay 1400
 #define ledWarning 13
-#define decimalPlaces 4
-#define aTx 12
-#define aRx 14
+#define BAUD_RATE 115200
+#define chipSelect D4
+#define buttonPin D8
 
-//variables
-int defaultFileCounter = 0;
-File Archivo;
-String defaultFileName = "datos"; // nombre del archivo por defecto
-String defaultFileExtension = ".csv";
+const String defaultFileName = "datos";
+const String defaultFileExtension = ".csv";
+
+//Configuraciones de red
+const char* ssid = "MIGUELANGEL";
+const char* password = "administrador5612";
+const char* mqtt_server = "192.168.1.87";
+const char* outTopic = "droneFenix/2/estacion1";
+
 String Data = "";
+int defaultFileCounter = 0,contadorDatos = 0;
+boolean newData = false, validData = false;
+File Archivo;
 
 
-// Web Server on port 80
-ESP8266WebServer server(80);
-SoftwareSerial arduinoSerial(aRx,aTx, false, 256);
+SoftwareSerial arduinoSerial(D3, D2, false, 256);// SoftwareSerial arduinoSerial(14, 12, false, 256);
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-void setup()
-{
-  if(initModule()){Serial.println("card initialized.");};
-  arduinoSerial.begin(115200);
-  pinMode(ledWarning,OUTPUT);
-  digitalWrite(ledWarning,LOW);
+void setup() {
+  Serial.begin(BAUD_RATE);
+  arduinoSerial.begin(BAUD_RATE);
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+  }
+  Serial.println("\nSoftware serial test started");
+  for (char ch = ' '; ch <= 'z'; ch++) {
+    arduinoSerial.write(ch);
+  }
+  arduinoSerial.println("");
   Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
   Serial.println("");
  IPAddress myIP = WiFi.softAPIP(); //Get IP address
   Serial.print("HotSpt IP:");
   Serial.println(myIP);
-  // Starting the web server
-  server.on("/", webPage);  
-  server.begin();
-  Serial.println("Web server running. Waiting for the ESP IP...");
-  while(SD.exists(defaultFileName+defaultFileCounter+defaultFileExtension)){
+  while(SD.exists(String(defaultFileName+defaultFileCounter+defaultFileExtension))){
     Serial.println("EXISTE: "+defaultFileName+defaultFileCounter+defaultFileExtension);
-    Serial.println(SD.exists(defaultFileName+defaultFileCounter+defaultFileExtension));  
+    Serial.println(SD.exists(String(defaultFileName+defaultFileCounter+defaultFileExtension)));  
     defaultFileCounter +=1;  
+    delay(defaultFileCounter);
   }
-  inicioDeAlmacenamiento(defaultFileName+defaultFileCounter+defaultFileExtension);// Esta funcion verifica la integridad del archivo y realiza la insecion de las caveceras
-
+  inicioDeAlmacenamiento(String(defaultFileName+defaultFileCounter+defaultFileExtension));// Esta funcion verifica la integridad del archivo y realiza la insecion de las caveceras
 }
 
-void loop()
-{
-  //getDataFromArduino();
-  //showData();
-  saveDataSD(Data);
-  smartDelay(timeDelay);
+void loop() {
+    if(newData){
+      ValidateAndShowData();
+    }
+    if(digitalRead(buttonPin)){
+      saveDataSD(Data);
+    }
+    smartDelay(timeDelay + contadorDatos); 
+}
+void getDataFromArduino(){
+  while (arduinoSerial.available() > 0) {
+    char inByte = arduinoSerial.read();
+    Data += inByte;
+    //Serial.write(inByte);
+    newData = true; 
+    validData = false; //to validate
+    yield();
+  }
+}
+void saveDataSD(String Data){
+  // String  var = getValue( StringVar, ',', 2); // if  a,4,D,r  would return D    
+  if(validData & newData){
+    Archivo = SD.open(String(defaultFileName+defaultFileCounter+defaultFileExtension), FILE_WRITE);
+    delay(1);
+    Archivo.println(Data);
+    delay(1);
+    Archivo.close();
+    contadorDatos++;
+    Data = "";newData = false;validData = false;
+  }
 }
 void smartDelay(int timeWait){
   unsigned long oldTime = millis();
   while(millis()-oldTime<=timeWait){
-    server.handleClient();
+    //server.handleClient();
+    char msg[Data.length()];
+    Data.toCharArray(msg,Data.length());
+    client.publish(outTopic, msg);
     getDataFromArduino();
+    delay(timeWait/10);
   }
 }
-
-void saveDataSD(String Data){
-  File dataFile = SD.open(defaultFileName+defaultFileCounter+defaultFileExtension, FILE_WRITE);
-  if (dataFile) {
-    dataFile.println(Data);
-    dataFile.close();
-    Serial.println(Data);
+void ValidateAndShowData(){
+  validData = false;
+  for(int i=0;i<=10;i++){
+    validData = getValue(Data, sep, i);
+    delay(10);
   }
+  if(!validData){Serial.println("Validacion: error");}
 }
-void getDataFromArduino(){
-    while (arduinoSerial.available() > 0) {
-      Serial.write(arduinoSerial.read());
-      yield();
-    }
-}
-
-String getData() {
-  Serial.print("out string: ");
-  Serial.println(Data);
-  return Data;
-}
-
-void showData(){
-  Serial.println ("------------ Received data -------------- ");
-    Serial.println (isnan(lat)?0:lat);
-    Serial.println (isnan(lon)?0:lon);
-    Serial.println (isnan(alt)?0:alt);
-    Serial.println (isnan(co2)?0:co2);
-    Serial.println (isnan(tvoc)?0:tvoc);
-    Serial.println (isnan(p)?0:p);
-    Serial.println (isnan(t)?0:t);
-    Serial.println (isnan(tempCCS)?0:tempCCS);
-    Serial.println (isnan(tBMP)?0:tBMP);
-    Serial.println (isnan(h)?0:h);
-    Serial.println (isnan(hic)?0:hic);
-}
-boolean initModule(){
-  Serial.begin(115200);
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    return false;
-  }
-  
-  return true;
-}
-
 void webPage(){
-            server.send(200, "text/plain",Data);
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println(minWebPageStart);
+            client.println(startData);
+            client.println(Data);
+            client.println(endData);
+            client.println(minWebPageEnd);
+
+            //client.println(,"Error loading page");
+            //server.send(200, "text/plain",Data);
             /*
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
@@ -143,15 +132,15 @@ void webPage(){
             client.println("*F</h3></body></html>");  
             */
 }
+
 void inicioDeAlmacenamiento(String fileNameAndExtension){
   Archivo = SD.open(fileNameAndExtension, FILE_WRITE);  
   Serial.println("EL ARCHIVO EN LA SD SE LLAMARA: "+fileNameAndExtension);
-  if (Archivo) {
     // latitud,longitud,altitud,temperatura (DHT), temperatura(CCS), temperatura (BMP), humedad (DHT11), hic (DHT11), co2 (CCS811), tvoc(CCS811), presionAtmosferica (BMP180)
 
     Archivo.println("SEP="+sep);
-    Archivo.print("------------------------------------------------ INICIO DE LA TOMA DE DATOS -------------------------------------------------");
-    Archivo.println();
+    //Archivo.print("------------------------------------------------ INICIO DE LA TOMA DE DATOS -------------------------------------------------");
+    //Archivo.println();
     
     Archivo.print("Latitud");
     Archivo.print(sep);
@@ -187,13 +176,40 @@ void inicioDeAlmacenamiento(String fileNameAndExtension){
 
     Archivo.print("presionAtmosferica (BMP180)");
     Archivo.println(sep);
-
-
-  }else {
-    digitalWrite(ledWarning, HIGH);
-    delay(500);
-    digitalWrite(ledWarning, LOW);
-    delay(500);
-  }
-  Archivo.close();
+    Archivo.close();
 }
+
+// String  var = getValue( StringVar, ',', 2); // if  a,4,D,r  would return D    
+bool getValue(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length();
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? data.substring(strIndex[0], strIndex[1]).toInt()>=0 : false;
+}  // END
+
+
+double getValueDouble(String data, char separator, int index)
+{
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length();
+
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
+    }
+    return found > index ? atof(data.substring(strIndex[0], strIndex[1]).c_str()) : 0;
+}  // END
+
