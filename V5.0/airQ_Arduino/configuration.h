5199625910233b4b74b11d98c261fd366c5c4938
miguel5612 <miguelangelcu@ufps.edu.co>
2019-04-18 18:26:28 -0500

    Fixed DHT22 to DHT11

--- V5.0/airQ_Arduino/configuration.h
+++ V5.0/airQ_Arduino/configuration.h
@@ -6,8 +6,8 @@
 //Area de configuracion del DHT11
 #define DHTPIN 8
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 //Area de configuracion del CCS
